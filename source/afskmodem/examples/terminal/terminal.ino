fcda3313171314a6f30041b4fe2ec1287cdac6f7
Wesley Carlsen <wcarlsen@linfirmware.com>
2015-11-02 21:34:43 -0500

    Terminal example

--- source/afskmodem/examples/terminal/terminal.ino
+++ source/afskmodem/examples/terminal/terminal.ino
@@ -1,9 +1,44 @@
-void setup() {
-  // put your setup code here, to run once:
+#include  <AudioModem.h> 
+#include  <ctype.h> 
 
+AudioModem modem; 
+
+void setup () 
+{ 
+  Serial.begin(57600); 
+  delay(1000); 
+  modem.begin (); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// Reverses the order of bits in a byte. 
+// I.e. MSB is swapped with LSB, etc. 
+unsigned char Bit_Reverse( unsigned char x ) 
+{ 
+    x = ((x >> 1) & 0x55) | ((x << 1) & 0xaa); 
+    x = ((x >> 2) & 0x33) | ((x << 2) & 0xcc); 
+    x = ((x >> 4) & 0x0f) | ((x << 4) & 0xf0); 
+    return x;    
+}
 
+void loop () 
+{ 
+  while ( modem . available ()) { 
+    unsigned char c = modem . read (); 
+    c = Bit_Reverse(c);
+    if ( isprint ( c )) { 
+      Serial . println (( char ) c ); 
+    } 
+    else { 
+      Serial . print ( "(" ); 
+      Serial . print ( c , HEX ); 
+      Serial . println ( ")" );       
+    } 
+  } 
+  if ( Serial . available ()) { 
+    modem . write ( 0xff ); 
+    while ( Serial . available ()) { 
+      char c =  Serial . read (); 
+      modem . write ( c ); 
+    } 
+  } 
 }
